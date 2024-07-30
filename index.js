4dbbcec8d427323881a2c73954a85cb263f7e5d8
Cameron Oberlin <oberlin2005@gmail.com>
2024-07-30 14:02:58 -0400

    Pushing so i can work on this more

---
+++ index.js
@@ -0,0 +1,98 @@
+require('dotenv').config();
+const OpenAI = require('openai');
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const openai = new OpenAI({apiKey: 'sk-proj-tWfLOlcUqPdTeG7XdkqnT3BlbkFJM4WyGPBaKHUQ3CpSwW3x'});
+let isMitchelOn = true;
+
+const threadMap = {};
+
+const getOpenAiThreadId = (discordThreadId) => {
+
+    return threadMap[discordThreadId];
+}
+
+const addThreadToMap = (discordThreadId, openAiThreadId) => {
+    threadMap[discordThreadId] = openAiThreadId;
+}
+
+const terminalStates = ["cancelled", "failed", "completed", "expired"];
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const statusCheckLoop = async (openAiThreadId, runId) => {
+    const run = await openai.beta.threads.runs.retrieve(
+        openAiThreadId,
+        runId
+    );
+
+    if (terminalStates.indexOf(run.status) < 0){
+        await sleep(1000);
+        return statusCheckLoop(openAiThreadId, runId);
+    }
+    console.log(run);
+
+    return run.status;
+} 
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log("Ai Mitchel is on and working");
+    client.user.setActivity({
+        name: 'Gooning to Sailor Moon',
+        type: ActivityType.Competing
+    });
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+    if (interaction.commandName === 'turn-on') {
+        isMitchelOn = !isMitchelOn;
+        console.log(isMitchelOn);
+        if (isMitchelOn) {
+            await interaction.reply('You have turned on Ai Mitchel. To start talking to him, start your next chat message with "Hey Mitchel".');
+        } else {
+            await interaction.reply('Mitchel AI is now turned off and he will no longer respond to anything anymore.');
+        }
+    }
+});
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot || !message.content || message.content === '') return;
+    if (message.content.toLowerCase().includes('hey mitchel')) {
+        const discordThreadId = message.channel.id;
+        let openAiThreadId = threadMap[discordThreadId];
+        if(!openAiThreadId){
+            const thread = await openai.beta.threads.create();
+            openAiThreadId = thread.id;
+            addThreadToMap(discordThreadId, openAiThreadId);
+        }
+        
+        
+        await openai.beta.threads.messages.create(
+            openAiThreadId,
+            {
+                role: "user",
+                content: message.content
+            }
+        );
+        const run = await openai.beta.threads.runs.create(
+            openAiThreadId,
+            {assistant_id: 'asst_RVub4n2Wr143zIoyqJzxWRWt'}
+        )
+        await statusCheckLoop(openAiThreadId, run.id || run.runId);
+
+
+        const messages = await openai.beta.threads.messages.list(openAiThreadId);
+        const aiResponse = messages.data[0].content[0].text.value;
+
+        message.reply(aiResponse);
+    }
+});
+
+client.login('MTI2NjQ0NzE1ODQ3MTgyMzM4Mg.GHoX0j.GbnR3KeGg_iZUDwlce8GMxVdfV3xBSkf6AFM30');
---
+++ registercommands.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+    {
+        name: 'turnon',
+        description: 'Turns on mitchel and will respond to anything with mitchel inside of the text'
+    }
+]
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Registering Slash Commands...')
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        )
+
+        console.log('Slash commands were registered successfully')
+    } catch (error) {
+        console.log(`there was an error ${error}`)
+    }
+})();
